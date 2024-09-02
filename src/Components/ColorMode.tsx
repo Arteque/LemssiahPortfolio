22c1a2bd484d579d1b5533d5ea3132f781b73fe2
Factum <art3que@gmail.com>
2024-09-03 00:59:42 +0200

    darkmode

--- src/Components/ColorMode.tsx
+++ src/Components/ColorMode.tsx
@@ -5,22 +5,35 @@ import { useState, useEffect } from "react"
 
 const ColorMode = () => {
 
+
+    useEffect(() => {
+        if(window.localStorage.portfolioTheme){
+            document.body.dataset.mode =  window.localStorage.portfolioTheme
+        }
+        
+    },[])
+
+
     const [darkmode, setDarkmode] = useState(false)
 
     useEffect(() => {
+        //Body dataset mode
         document.body.dataset.mode  = darkmode ? "dark" : "light"
+        window.localStorage.portfolioTheme = darkmode ? "dark" : "light"
     },[darkmode])
+
+   
     
     return  <div className={`color-mode flex`} >
-                <input type="checkbox" id="checkdark" name="checkdark" className="color-mode__checkbox" 
+                <input type="checkbox" id="checkdark" name="checkdark" className="color-mode__checkbox hidden" 
                 
                 onChange={(e) => {setDarkmode(e.target.checked)}}/>
                 <label htmlFor="checkdark" className="h-6 overflow-hidden">
                     <span className="color-mode__dark block translate-y-[-26px] dark:translate-y-[0]">
-                        <FontAwesomeIcon icon={faMoon} className="text-second" /> <span className="text-second-100">Darkmode</span>
+                        <FontAwesomeIcon icon={faMoon} className="text-second" /> <span className="text-second-100 font-medium uppercase">Darkmode</span>
                     </span>
                     <span className="color-mode__light block translate-y-[-26px] dark:translate-y-[0]">
-                        <FontAwesomeIcon icon={faSun} className="text-prime" /> <span className="text-prime-100">Lightmode</span>
+                        <FontAwesomeIcon icon={faSun} className="text-prime" /> <span className="text-prime-100 font-medium uppercase">Lightmode</span>
                     </span>
                 </label>
             </div>
