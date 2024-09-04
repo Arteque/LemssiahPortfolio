f7ee43374d6ab88c058a8d64d5426b757e4c1b20
Factum <art3que@gmail.com>
2024-09-04 02:15:18 +0200

    card section component

---
+++ src/Components/Assets/Card.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import { cva,  VariantProps} from "class-variance-authority";
+import { cn } from "../../libs/utils";
+
+
+const cardVariants = cva(
+    "max-w-[450px] p-5  rounded-lg  border-bg bg-bg-100 ",
+    {
+        variants:{
+            variant:{
+                default: "transition hover:box-shadow-sm",
+                mediaCard: "p-0 border-none"
+            },
+        },
+        defaultVariants:{
+            variant:"default",
+        }
+    }
+)
+
+
+interface cardProps extends VariantProps<typeof cardVariants>{
+    className:string
+    children:any 
+}
+
+const Card:FC<cardProps> = ({className,variant, children}) => {
+    return <div className={`${cn(cardVariants({variant}), className)}`} >
+        {children}
+    </div>
+}
+
+export default Card
--- src/Pages/Start.tsx
+++ src/Pages/Start.tsx
@@ -1,7 +1,8 @@
-import { Button, buttonVariants } from '../Components/Assets/Button'
+import {  buttonVariants } from '../Components/Assets/Button'
 import Container from '../Components/Container'
 import { Link } from 'react-router-dom'
 import Header from '../Components/Header'
+import Card from '../Components/Assets/Card'
 
 function Start() {
   
@@ -40,29 +41,28 @@ function Start() {
           </Header>
           <Container>
             <div className="section__content flex gap-2 justify-between">
-              <div className="card--media max-w-[450px] p-5  rounded-lg  border-bg-100 bg-bg-100">
-                  <div className="card__header relative">
-                     <img className="w-[50%] my-10 mx-auto block" src="./cartoonFinder.svg" alt="Ideen Erfinder" />
-                     <div className="mediacollection absolute top-0 left-0 w-full h-full animate-spin-slower">
-                        <img className="block rotate-10 origin-center animate-spin-slow  absolute top-0 left-[50%]  max-w-[120px] max-h-[120px]" src="./Erfinder/Element 3.svg" alt="Analyse" />
-                        <img className="block rotate-180 origin-center animate-spin-slow  absolute top-[50%] left-0 max-w-[120px] max-h-[120px]" src="./Erfinder/LaborFlasche1.svg" alt="Laborflasche" />
-                        <img className="block rotate-45 origin-center animate-spin-slow  absolute top-[50%] right-5 max-w-[120px] max-h-[120px]" src="./Erfinder/LaborMischFlasche1.svg" alt="Labor Misch Flasche" />
-                        <img className="block rotate-100 origin-center animate-spin-slow  absolute bottom-0 left-[50%] max-w-[120px] max-h-[120px]" src="./Erfinder/Mikroskop.svg" alt="Labor Misch Flasche" />
-                     </div>
-                  </div>
-                  <div className="card__body bg-bg-200/[1] p-5  mb-5 z-10 backdrop-blur-lg">
-                    <div className="card__title">
-                        <h3 className="card__title text-center text-[2rem]"><span className="text-prime">Ideen</span><span className="text-second font-black uppercase">erfinder</span></h3>
+              <Card variant="default" className="card">
+                <div className="card__header relative">
+                      <img className="w-[50%] my-10 mx-auto block" src="./cartoonFinder.svg" alt="Ideen Erfinder" />
+                      <div className="mediacollection absolute top-0 left-0 w-full h-full animate-spin-slower">
+                          <img className="block rotate-10 origin-center animate-spin-slow  absolute top-0 left-[50%]  max-w-[120px] max-h-[120px]" src="./Erfinder/Element 3.svg" alt="Analyse" />
+                          <img className="block rotate-180 origin-center animate-spin-slow  absolute top-[50%] left-0 max-w-[120px] max-h-[120px]" src="./Erfinder/LaborFlasche1.svg" alt="Laborflasche" />
+                          <img className="block rotate-45 origin-center animate-spin-slow  absolute top-[50%] right-5 max-w-[120px] max-h-[120px]" src="./Erfinder/LaborMischFlasche1.svg" alt="Labor Misch Flasche" />
+                          <img className="block rotate-100 origin-center animate-spin-slow  absolute bottom-0 left-[50%] max-w-[120px] max-h-[120px]" src="./Erfinder/Mikroskop.svg" alt="Labor Misch Flasche" />
+                      </div>
                     </div>
-                    <div className="card__text">
-                      <p className="text-text-100 font-medium my-3">
-                        Durch Besprechungen mit den Kunden werden die Ziele und die hinein Wege skiziert.  <br />
-                        Anhand von was erwartet ist und was im Webberreich möglich ist, werden Ideen und Konzepte Entwickelt!
-                      </p>
+                    <div className="card__body bg-bg-200/[1] p-5  mb-5 z-10 backdrop-blur-lg">
+                      <div className="card__title">
+                          <h3 className="card__title text-center text-[2rem]"><span className="text-prime">Ideen</span><span className="text-second font-black uppercase">erfinder</span></h3>
+                      </div>
+                      <div className="card__text">
+                        <p className="text-text-100 font-medium my-3">
+                          Durch Besprechungen mit den Kunden werden die Ziele und die hinein Wege skiziert.  <br />
+                          Anhand von was erwartet ist und was im Webberreich möglich ist, werden Ideen und Konzepte Entwickelt!
+                        </p>
+                      </div>
                     </div>
-                  </div>
-
-              </div>
+              </Card>
               <div className="card--media max-w-[450px] p-5  rounded-lg  border-bg-100 bg-bg-100">
                   <div className="card__header relative">
                      <img className="w-[50%] my-10 mx-auto block" src="./cartoonDesign.svg" alt="Ideen Künstler" />
