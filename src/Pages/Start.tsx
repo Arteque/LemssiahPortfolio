c95c7b3bf366eea310581e30ffd959c6c299ea5d
Ahmed Lemssiah <art3que@gmail.com>
2024-09-19 16:13:26 +0200

    Project JSON File

--- src/Pages/Start.tsx
+++ src/Pages/Start.tsx
@@ -1,14 +1,12 @@
-import {  Button, buttonVariants } from '../Components/Assets/Button'
+import {  buttonVariants } from '../Components/Assets/Button'
 import Container from '../Components/Container'
 import { Link } from 'react-router-dom'
 import Header from '../Components/Header'
 import Card from '../Components/Assets/Card'
 import Services from "../Data/Services.json"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGlobeAfrica, faSave, faShareAlt, faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
-import { faCss3, faHtml5, faJs, faReact } from '@fortawesome/free-brands-svg-icons'
+import Projects from "../Data/Projects.json"
 import Youtube from '../Components/Youtube'
-
+import ProjectCard from '../Components/ProjectCard'
 
 const Start = () =>  {
 
@@ -110,213 +108,11 @@ const Start = () =>  {
           </Header>
           <Container>
               <div className='md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-10'>
-                <Card variant="mediaCard" className="project relative group cursor-pointer  overflow-hidden bg-bg my-5 
-                shadow-sm shadow-prime-200 group-hover:shadow-lg ">
-                  <div className="card__header overflow-hidden">
-                      <img src="./Projects/DefaultThumb.jpg" alt="DefaultThumb" className="
-                      transition-all duration-[5s] grayscale
-                      group-hover:scale-[1.01] group-hover:grayscale-0"/>
-                  </div>
-                  <div className="card__body">
-                    <h3 className="text-prime text-lg flex gap-2 pt-5 px-3">
-                      <a href="#" title="project-title">
-                        <span className="card__icon">
-                          <FontAwesomeIcon icon={faGlobeAfrica} />
-                        </span>
-                        <span className="card__text">
-                        Project Überschrift |
-                        <span  className="text-text-200 text-xs"> 
-                        <small> Marokko</small> - <small>29.11.2024</small>
-                        </span>
-                        </span>
-                      </a>
-                    </h3>
-                    <h4 className="px-3 text-xs text-text-200">
-                      <span>100 Views</span> | <span>10 Likes</span>
-                    </h4>
-                    <p className="card__description p-5 text-prime">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ipsam amet vitae, esse expedita eaque eius ducimus aspernatur sunt? Soluta.
-                    </p>
-                  </div>
-                  <div className="card__footer">
-                    <ul className='absolute flex flex-col gap-2 top-2 right-[-20%] transition-all duration-[.5s]
-                    group-hover:right-0 
-                    '>
-                      <li>
-                        <Button variant="roundedLight">
-                            <FontAwesomeIcon icon={faThumbsUp}  />
-                        </Button>
-                      </li>
-                      <li>
-                        <Button variant="roundedLight">
-                            <FontAwesomeIcon icon={faThumbsDown}  />
-                        </Button>
-                      </li>
-                      <li>
-                        <Button variant="roundedLight">
-                            <FontAwesomeIcon icon={faShareAlt}  />
-                        </Button>
-                      </li>
-                      <li>
-                        <Button variant="roundedLight">
-                            <FontAwesomeIcon icon={faSave}  />
-                        </Button>
-                      </li>
-                    </ul>
-                    <ul className="card__tek flex justify-end p-2 gap-1 text-text-100">
-                      <li>
-                        <FontAwesomeIcon icon={faHtml5} />
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faCss3} />
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faJs} />
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faReact} />
-                      </li>
-                    </ul>
-                  </div>
-                </Card>
-                <Card variant="mediaCard" className="project relative group cursor-pointer  overflow-hidden bg-bg my-5 
-                shadow-sm shadow-prime-200 group-hover:shadow-lg ">
-                  <div className="card__header overflow-hidden">
-                      <img src="./Projects/DefaultThumb.jpg" alt="DefaultThumb" className="
-                      transition-all duration-[5s] grayscale
-                      group-hover:scale-[1.01] group-hover:grayscale-0"/>
-                  </div>
-                  <div className="card__body">
-                    <h3 className="text-prime text-lg flex gap-2 pt-5 px-3">
-                      <a href="#" title="project-title">
-                        <span className="card__icon">
-                          <FontAwesomeIcon icon={faGlobeAfrica} />
-                        </span>
-                        <span className="card__text">
-                        Project Überschrift |
-                        <span  className="text-text-200 text-xs"> 
-                        <small> Marokko</small> - <small>29.11.2024</small>
-                        </span>
-                        </span>
-                      </a>
-                    </h3>
-                    <h4 className="px-3 text-xs text-text-200">
-                      <span>100 Views</span> | <span>10 Likes</span>
-                    </h4>
-                    <p className="card__description p-5 text-prime">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ipsam amet vitae, esse expedita eaque eius ducimus aspernatur sunt? Soluta.
-                    </p>
-                  </div>
-                  <div className="card__footer">
-                    <ul className='absolute flex flex-col gap-2 top-2 right-[-20%] transition-all duration-[.5s]
-                    group-hover:right-0 
-                    '>
-                      <li>
-                        <Button variant="roundedLight">
-                            <FontAwesomeIcon icon={faThumbsUp}  />
-                        </Button>
-                      </li>
-                      <li>
-                        <Button variant="roundedLight">
-                            <FontAwesomeIcon icon={faThumbsDown}  />
-                        </Button>
-                      </li>
-                      <li>
-                        <Button variant="roundedLight">
-                            <FontAwesomeIcon icon={faShareAlt}  />
-                        </Button>
-                      </li>
-                      <li>
-                        <Button variant="roundedLight">
-                            <FontAwesomeIcon icon={faSave}  />
-                        </Button>
-                      </li>
-                    </ul>
-                    <ul className="card__tek flex justify-end p-2 gap-1 text-text-100">
-                      <li>
-                        <FontAwesomeIcon icon={faHtml5} />
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faCss3} />
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faJs} />
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faReact} />
-                      </li>
-                    </ul>
-                  </div>
-                </Card>
-                <Card variant="mediaCard" className="project relative group cursor-pointer  overflow-hidden bg-bg my-5 
-                shadow-sm shadow-prime-200 group-hover:shadow-lg ">
-                  <div className="card__header overflow-hidden">
-                      <img src="./Projects/DefaultThumb.jpg" alt="DefaultThumb" className="
-                      transition-all duration-[5s] grayscale
-                      group-hover:scale-[1.01] group-hover:grayscale-0"/>
-                  </div>
-                  <div className="card__body">
-                    <h3 className="text-prime text-lg flex gap-2 pt-5 px-3">
-                      <a href="#" title="project-title">
-                        <span className="card__icon">
-                          <FontAwesomeIcon icon={faGlobeAfrica} />
-                        </span>
-                        <span className="card__text">
-                        Project Überschrift |
-                        <span  className="text-text-200 text-xs"> 
-                        <small> Marokko</small> - <small>29.11.2024</small>
-                        </span>
-                        </span>
-                      </a>
-                    </h3>
-                    <h4 className="px-3 text-xs text-text-200">
-                      <span>100 Views</span> | <span>10 Likes</span>
-                    </h4>
-                    <p className="card__description p-5 text-prime">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ipsam amet vitae, esse expedita eaque eius ducimus aspernatur sunt? Soluta.
-                    </p>
-                  </div>
-                  <div className="card__footer">
-                    <ul className='absolute flex flex-col gap-2 top-2 right-[-20%] transition-all duration-[.5s]
-                    group-hover:right-0 
-                    '>
-                      <li>
-                        <Button variant="roundedLight">
-                            <FontAwesomeIcon icon={faThumbsUp}  />
-                        </Button>
-                      </li>
-                      <li>
-                        <Button variant="roundedLight">
-                            <FontAwesomeIcon icon={faThumbsDown}  />
-                        </Button>
-                      </li>
-                      <li>
-                        <Button variant="roundedLight">
-                            <FontAwesomeIcon icon={faShareAlt}  />
-                        </Button>
-                      </li>
-                      <li>
-                        <Button variant="roundedLight">
-                            <FontAwesomeIcon icon={faSave}  />
-                        </Button>
-                      </li>
-                    </ul>
-                    <ul className="card__tek flex justify-end p-2 gap-1 text-text-100">
-                      <li>
-                        <FontAwesomeIcon icon={faHtml5} />
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faCss3} />
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faJs} />
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faReact} />
-                      </li>
-                    </ul>
-                  </div>
-                </Card>
+                {
+                  Projects && Projects.map((item) => (
+                    <ProjectCard key={`${item.title}${item.id}`}/>
+                  ))
+                }
               </div>
           </Container>
         </section>
