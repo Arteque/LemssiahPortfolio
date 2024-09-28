55d66f4233c438d991230bb9db79b03eb0eadfde
Factum <art3que@gmail.com>
2024-09-29 01:33:27 +0200

    Error page

--- src/Pages/Error.tsx
+++ src/Pages/Error.tsx
@@ -1,15 +1,36 @@
 import { useRouteError } from "react-router-dom"
+import Container from "../Components/Container"
+import MainHeader from "../Components/MainHeader"
+import { useState } from "react"
+import MainFooter from "../Components/MainFooter"
 
 function Error() {
     const error:unknown = useRouteError()
     console.log(error)
+
+
+      //FormState
+  const [formState, setFormState] = useState(false)
+
+  const changetheFormState = () => {
+    setFormState(prev => !prev)
+  }
+
   return (
     <>
-        <h2>Irgendwas stimmt nicht!</h2>
-        <p>
-            {(error as Error)?.message ||
-            (error as { statusText?: string })?.statusText}
-        </p>
+        <MainHeader toggleForm={changetheFormState}/>
+        <main>
+          <section className="min-h-[68svh]">
+            <Container>
+                      <h2 className="text-center my-10 text-2xl font-black text-prime">Irgendwas stimmt nicht!</h2>
+                    <p className="max-w-[400px] mx-auto text-center text-prime-100">
+                        {(error as Error)?.message ||
+                        (error as { statusText?: string })?.statusText}
+                    </p>
+            </Container>
+          </section>
+        </main>
+        <MainFooter showkontakt={formState} toggleForm={changetheFormState}/>
     </>
   )
 }
